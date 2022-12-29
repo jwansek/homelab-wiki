be7fa4f59ff3dcf9103c030f028c1efea1a8db02
jwansek <eddie.atten.ea29@gmail.com>
2022-12-29 19:13:04 +0000

    Updated icon

--- LocalSettings.php
+++ LocalSettings.php
@@ -38,8 +38,7 @@ $wgResourceBasePath = $wgScriptPath;
 ## The URL paths to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
 $wgLogos = [
-	'1x' => "/images/c/c9/Logo.png",
-	'icon' => "$wgResourceBasePath/resources/assets/change-your-logo.svg",
+	'1x' => "$wgResourceBasePath/images/c/c9/Logo.png",
 ];
 
 ## UPO means: this is also a user preference option
