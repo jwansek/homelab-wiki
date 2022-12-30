18165273c56eb0143d141e871ea5267ce471be6d
jwansek <eddie.atten.ea29@gmail.com>
2022-12-30 03:08:01 +0000

    Updated

--- LocalSettings.php
+++ LocalSettings.php
@@ -30,7 +30,7 @@ $wgMetaNamespace = "Eden's_Homelab_Wiki";
 $wgScriptPath = "";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "https://eda.gay/wiki/";
+$wgServer = "https://eda.gay/wiki";
 
 ## The URL path to static resources (images, scripts, etc.)
 $wgResourceBasePath = $wgScriptPath;
